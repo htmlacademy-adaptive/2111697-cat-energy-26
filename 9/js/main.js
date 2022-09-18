52f38971b7fe2668be02ff6a9c0e49c07658bff3
Keks <keks@htmlacademy.ru>
2022-09-18 13:13:55 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,1 @@
+const navToggler=document.querySelector(".main-nav__toggle"),navigation=document.querySelector(".main-nav__list");navToggler.hidden=!1,navigation.classList.add("main-nav__list--enabled"),navToggler.addEventListener("click",(()=>{navigation.classList.toggle("main-nav__list--opened"),navToggler.classList.toggle("main-nav__toggle--opened")}));
